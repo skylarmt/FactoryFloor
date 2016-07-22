ad4bf0b44d6a750d0aafc579a2586e13e84087bf
Skylar Ittner <admin@netsyms.com>
2016-07-21 20:20:17 -0600

    Add snarky bubble when there aren't any messages.

--- dogetmsgs.php
+++ dogetmsgs.php
@@ -10,35 +10,39 @@ redirectifnotloggedin();
 $messages = $database->query('SELECT * FROM messages ORDER BY messagedate DESC LIMIT 10')->fetchAll();
 //var_dump($messages);
 //$messages = array_reverse($messages); // Show newest on top
-foreach ($messages as $msg) {
-    ?>
-    <div class="panel panel-default">
-        <div class="panel-body">
-            <?php echo strip_tags($msg['messagetext']); ?>
-        </div>
-        <div class="panel-footer">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                    <i class="fa fa-user"></i> <?php echo realnamefromid($msg['senderid']); ?>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-6" style="text-align: right;">
-                    <i class='fa fa-clock-o'></i> <?php echo date("F j, Y, g:i a", strtotime($msg['messagedate'])); ?>
-                    <?php
-                    if (userrolefromid($_SESSION['userid']) > 0) {
-                        ?>
-                        <form style="display: inline-block;"
-                              action="/dodeletemsg.php" method="GET"
-                              onsubmit="$('#delmsgbtn<?php echo $msg['messageid']; ?>').prop('disabled', true);">
-                            <input type="hidden" name="msgid" value="<?php echo $msg['messageid']; ?>" />
-                            <button type="submit" id="delmsgbtn<?php echo $msg['messageid']; ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
-                        </form>
+if (count($messages) > 0) {
+    foreach ($messages as $msg) {
+        ?>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <?php echo strip_tags($msg['messagetext']); ?>
+            </div>
+            <div class="panel-footer">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-6">
+                        <i class="fa fa-user"></i> <?php echo realnamefromid($msg['senderid']); ?>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-6" style="text-align: right;">
+                        <i class='fa fa-clock-o'></i> <?php echo date("F j, Y, g:i a", strtotime($msg['messagedate'])); ?>
                         <?php
-                    }
-                    ?>
+                        if (userrolefromid($_SESSION['userid']) > 0) {
+                            ?>
+                            <form style="display: inline-block;"
+                                  action="/dodeletemsg.php" method="GET"
+                                  onsubmit="$('#delmsgbtn<?php echo $msg['messageid']; ?>').prop('disabled', true);">
+                                <input type="hidden" name="msgid" value="<?php echo $msg['messageid']; ?>" />
+                                <button type="submit" id="delmsgbtn<?php echo $msg['messageid']; ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+                            </form>
+                            <?php
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php
+        <?php
+    }
+} else {
+    echo "<div class='alert alert-info'><i class='fa fa-commenting-o'></i> It's very quiet in here.  A little <i>too</i> quiet, perhaps?</div>";
 }
 ?>
